d9772dbd59499319d9475a3f4ac5adc8ea048605
A1Baby <masulover4.5.21@gmail.com>
2025-08-16 23:18:09 +0545

    Update QR-Generator.py

--- QR-Generator.py
+++ QR-Generator.py
@@ -1,8 +1,5 @@
-[]:
-pip install wifi qrcode generator
-[1]:
 from wifi_qrcode_generator import wifi_qrcode
 qr_code = wifi_qrcode("clcoding", hidden=False,
           authentication_type="WPA", password="CLB422F061")
 qr_code_image = qr_code.make_image()
-qr_code.img.save("wifi_qr_code.png")
+qr_code_image.save("wifi_qr_code.png")
